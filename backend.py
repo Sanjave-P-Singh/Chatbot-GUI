35bd22343b08d1b838eed58a7f9ac05485bf3a98
sanjave <sanjave.singh4@gmail.com>
2024-06-02 21:56:34 -0400

    Connecting the Chatbot to the GUI

--- backend.py
+++ backend.py
@@ -1,6 +1,5 @@
 import openai
 
-
 class Chatbot:
     def __init__(self):
         openai.api_key = "sk-proj-eM6BwM7XGJTVXLXQFcsYT3BlbkFJIo5hdGOkT6ZXmtJGoZ1P"
@@ -9,7 +8,7 @@ class Chatbot:
         response = openai.Completion.create(
             engine="text-davinci-003",
             prompt=user_input,
-            max_tokens=4000,
+            max_tokens=3000,
             temperature=0.5
         ).choices[0].text()
         return response
